921accfaf064dbb6d435df07d676ad7fe999830e
Daniel Uribe <duribe8@gmail.com>
2015-06-07 19:10:28 -0700

    Updated production gems

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ActiveRecord::Base
   belongs_to :list
 
   def days_left
-    6 - (DateTime.now.to_date - created_at.to_date).to_i
+    ((created_at.to_date + 6) - DateTime.now.to_date).to_i
   end
 
 end
